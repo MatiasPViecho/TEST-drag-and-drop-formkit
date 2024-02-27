b5ada6a6e21d149c40e11ded42266cc189ec18e8
MatiasPViecho <mpviecho@gmail.com>
2024-02-27 20:49:48 -0300

    feat: add active style, add animation plugin

--- components/Draggable.tsx
+++ components/Draggable.tsx
@@ -1,26 +1,31 @@
 'use client';
+import { animations } from '@formkit/drag-and-drop';
+
 import { useDragAndDrop } from '@formkit/drag-and-drop/react';
-import { Fragment } from 'react';
 import { Item } from './Item';
 export const Draggable = () => {
-  const [parent, texts] = useDragAndDrop<HTMLUListElement, string>([
-    'Text NUMBER 1',
-    'Text NUMBER 2',
-    'Text NUMBER 3',
-    'Text NUMBER 4',
-    'Text NUMBER 5',
-  ]);
+  const [parent, texts] = useDragAndDrop<HTMLUListElement, string>(
+    [
+      'Text NUMBER 1',
+      'Text NUMBER 2',
+      'Text NUMBER 3',
+      'Text NUMBER 4',
+      'Text NUMBER 5',
+    ],
+    { plugins: [animations()] }
+  );
   return (
     <ul
       ref={parent}
       className="flex flex-row gap-3 p-24"
     >
       {texts.map((text) => (
-        <Fragment key={text}>
-          <li className="peer peer-active:bg-blue-800 p-4 bg-blue-700">
-            <Item>{text}</Item>
-          </li>
-        </Fragment>
+        <li
+          key={text}
+          className={`p-4 bg-blue-700 active:bg-opacity-20`}
+        >
+          <Item>{text}</Item>
+        </li>
       ))}
     </ul>
   );
