f0d3aff6c4eb2cffd41c55e0b185fb97a41b248f
MatiasPViecho <mpviecho@gmail.com>
2024-02-27 21:13:25 -0300

    feat: add drag and drop different lists

--- components/Draggable.tsx
+++ components/Draggable.tsx
@@ -1,32 +1,68 @@
 'use client';
-import { animations } from '@formkit/drag-and-drop';
+import { animations, multiDrag, selections } from '@formkit/drag-and-drop';
 
 import { useDragAndDrop } from '@formkit/drag-and-drop/react';
 import { Item } from './Item';
 export const Draggable = () => {
-  const [parent, texts] = useDragAndDrop<HTMLUListElement, string>(
-    [
-      'Text NUMBER 1',
-      'Text NUMBER 2',
-      'Text NUMBER 3',
-      'Text NUMBER 4',
-      'Text NUMBER 5',
-    ],
-    { plugins: [animations()] }
-  );
+  const texts = [
+    'Text NUMBER 1',
+    'Text NUMBER 2',
+    'Text NUMBER 3',
+    'Text NUMBER 4',
+    'Text NUMBER 5',
+  ];
+  const [parent1, texts1] = useDragAndDrop<HTMLUListElement, string>(texts, {
+    group: 'A',
+    // plugins: [
+    //   multiDrag({
+    //     plugins: [
+    //       selections({
+    //         selectedClass: 'bg-blue-800 border-red-400 border-2',
+    //       }),
+    //     ],
+    //   }),
+    // ],
+  });
+  const [parent2, texts2] = useDragAndDrop<HTMLUListElement, string>([], {
+    group: 'A',
+    // plugins: [
+    //   multiDrag({
+    //     plugins: [
+    //       selections({
+    //         selectedClass: 'bg-blue-800 border-red-400 border-2',
+    //       }),
+    //     ],
+    //   }),
+    // ],
+  });
   return (
-    <ul
-      ref={parent}
-      className="flex flex-row gap-3 p-24"
-    >
-      {texts.map((text) => (
-        <li
-          key={text}
-          className={`p-4 bg-blue-700 active:bg-opacity-20`}
-        >
-          <Item>{text}</Item>
-        </li>
-      ))}
-    </ul>
+    <div className="flex flex-row gap-8 px-8 py-2 bg-slate-600 w-full justify-center">
+      <ul
+        ref={parent1}
+        className="flex flex-row gap-3 flex-1 bg-violet-700 p-2"
+      >
+        {texts1.map((text) => (
+          <li
+            key={text}
+            className={`p-4 bg-pink-700 active:bg-opacity-20`}
+          >
+            <Item>{text}</Item>
+          </li>
+        ))}
+      </ul>
+      <ul
+        ref={parent2}
+        className="flex flex-row gap-3 bg-red-500 flex-1 p-2"
+      >
+        {texts2.map((text) => (
+          <li
+            key={text}
+            className={`p-4 bg-blue-700 active:bg-opacity-20`}
+          >
+            <Item>{text}</Item>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
